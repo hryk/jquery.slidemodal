530e4f1fea01e4bcc5e00642bc7f8d3d9ea3d4f6
Hiroyuki Nakamura <nakamura@spiber.jp>
2012-06-21 15:17:06 +0900

    supported 'width: full'

---
+++ demos/snippets/01_specify_width.js
@@ -0,0 +1,3 @@
+var modal = $('#modal').slide_modal({ width: 500 });
+$('.modal_open').click(function(){ modal.slide_modal('open'); });
+$('.modal_close').click(function(){ modal.slide_modal('close'); });
